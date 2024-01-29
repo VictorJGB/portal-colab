ddda727e84be86eddf83b193f289f66edb1c016a
victor <victorgb.dev@gmail.com>
2024-01-29 12:27:39 -0300

    style: responsive layout

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <div className="flex w-full min-h-screen flex-col items-center justify-center">
 
       {/* Utils section */}
-      <section className='h-screen w-full flex flex-col items-center justify-around'>
+      <section className='h-[200vh] lg:h-screen w-full flex flex-col items-center justify-around'>
         <div className='flex flex-col gap-2 items-center justify-center'>
           <h1 className='text-primary text-2xl font-bold'>Portal do Colaborador</h1>
           <h2 className='text-secondary text-lg font-semibold'>Tudo o que você precisa em um só lugar</h2>
@@ -35,7 +35,7 @@ export default function Home() {
       </section>
 
       {/* Training section */}
-      <section className='h-screen bg-primary w-full flex flex-col items-center justify-around'>
+      <section className='h-screen bg-primary w-full flex flex-col gap-10 lg:gap-20 items-center justify-center'>
         <h2 className='text-white text-lg font-semibold'>Links de Treinamento</h2>
         <div className='flex w-full gap-6 flex-wrap items-center justify-center'>
           {/* Card link */}
@@ -53,7 +53,7 @@ export default function Home() {
       </section>
 
       {/* Department section */}
-      <section className='h-screen w-full flex flex-col items-center justify-around'>
+      <section className='h-[200vh] lg:h-screen w-full flex flex-col gap-10 items-center justify-center'>
         <h2 className='text-zinc-600 text-lg font-semibold'>Links de Setores</h2>
         <div className='flex w-full gap-6 flex-wrap items-center justify-center'>
           {/* Card link */}
